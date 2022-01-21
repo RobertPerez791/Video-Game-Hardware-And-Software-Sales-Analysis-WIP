105a06747e00c80aae0c8e041c8da9682741f9fc
RobertPerez791 <85271816+RobertPerez791@users.noreply.github.com>
2022-01-20 23:37:46 -0600

    Add files via upload

---
+++ Consoles.py
@@ -0,0 +1,76 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+
+
+
+rec_count = 0
+
+# The values that'll be extracted from the wikipedia table
+platform = []
+type = []
+firm = []
+release = []
+units_sold = []
+
+
+# Setting up extraction of table
+surl = 'https://en.wikipedia.org/wiki/List_of_best-selling_game_consoles'
+html_text = requests.get(surl).text
+soup = BeautifulSoup(html_text, 'lxml')
+chart = soup.find('div', id='mw-content-text').find('table')
+
+# Extracting data from the table
+for row in chart.find_all('tr')[1:]:
+        col = row.find_all('td')
+        column1 = (col[0].find('a')['title'].strip())   # Returns the name of the platform from the table.
+        column2 = col[1].string.strip()                 # Returns whether the console is a handheld or home console.
+        column3 = col[2].find('a')['title'].strip()     # Returns the console's manufacturing firm.
+        column4 = col[3].string.strip()                 # Returns the console's release year.
+
+        # If the row contains a <sup> tag or a <small> tag, col[4].string will return None
+        # An if else statement is used to handle different tags
+
+        if col[4].string is not None:
+                column5 = col[4].string         # Returns the console's units sold in millions, if there is no <sup> or <small> tag in the column
+        else:                                   # A second if else statement is used to handle the row, depending on whether it has a <sup> tag or a <small> tag
+                if 'million' in col[4].next:    # Returns units sold if row contains a <small> tag
+                        column5 = col[4].next
+                else:                           # Returns units sold if row contains a <sup> tag
+                        column5 = col[4].next.next.next.next
+
+        # Removes text from the units sold and leaves just the raw number
+        column5 = (column5.replace('million',''))
+        column5 = (column5.replace('\n', ''))
+
+        # Converts string into a numerical format with 2 decimal places.
+        # Exception line covers cases where units sold lies in a range (eg. "82-84")
+        try:
+                column5 = round(float(column5),2)
+        except:
+                column5 = round(float(column5[:2]),2)
+
+
+        # Adds data to all lists created earlier
+        platform.append(column1)
+        type.append(column2)
+        firm.append(column3)
+        release.append(column4)
+        units_sold.append(column5)
+
+        rec_count += 1
+
+# Formatting columns for the database
+columns = {'Platform': platform, 'Type': type, 'Firm': firm, 'Release': release, 'Units Sold (in Millions)': units_sold}
+
+print (rec_count)
+
+# Ensuring "units_sold" column is in numeric format
+df = pd.DataFrame(columns)
+df[["Units Sold (in Millions)"]] = df[["Units Sold (in Millions)"]].apply(pd.to_numeric)
+
+print(df)
+df = df[['Platform', 'Type', 'Firm', 'Release', 'Units Sold (in Millions)']]
+df.index.name = None
+df.to_csv("BestSellingConsoles.csv", sep=",", encoding='utf-8-sig')
---
+++ vgchartz.py
@@ -0,0 +1,100 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+
+pages = 20
+rec_count = 0
+
+# The values that'll be extracted from the vgchartz database.
+
+rank = []
+gname = []
+platform = []
+year = []
+critic_score = []
+publisher = []
+developer = []
+sales_na = []
+sales_eu = []
+sales_jp = []
+sales_ot = []
+sales_gl = []
+
+
+urlhead = 'http://www.vgchartz.com/gamedb/?page='
+urltail = '&console=&region=All&developer=&publisher=&genre=&boxart=Both&ownership=Both'
+urltail += '&results=1000&order=Sales&showtotalsales=0&showtotalsales=1&showpublisher=0'
+urltail += '&showpublisher=1&showvgchartzscore=0&shownasales=1&showdeveloper=1&showcriticscore=1'
+urltail += '&showpalsales=0&showpalsales=1&showreleasedate=1&showuserscore=0&showjapansales=1'
+urltail += '&showlastupdate=0&showothersales=1&showgenre=1&sort=GL'
+
+
+# For-loop will allow for scraping of multiple pages
+for page in range(1, 20):
+
+    # Setting up extraction of table
+    surl = urlhead + str(page) + urltail
+    html_text = requests.get(surl).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    chart = soup.find('div', id='generalBody').find('table')
+
+    # Extracting data from the table
+    for row in chart.find_all('tr')[3:]:
+        try:
+            col = row.find_all('td')
+
+        # extract data into column data
+            column_1 = col[0].string.strip()
+            column_2 = col[2].find('a').string.strip()
+            column_3 = col[3].find('img')['alt'].strip()
+            column_4 = col[4].string.strip() #publisher
+            column_5 = col[5].string.strip() #developer
+            column_6 = col[6].string.strip() #Critic Score
+
+            # Replace will remove non-numerical characters from columns for easier conversion
+            column_7 = col[7].string.strip().replace("m","")
+            column_8 = col[8].string.strip().replace("m","")
+            column_9 = col[9].string.strip().replace("m","")
+            column_10 = col[10].string.strip().replace("m","")
+            column_11 = col[11].string.strip().replace("m","")
+
+        # Add Data to columns
+        # Adding data only if able to read all of the columns
+            rank.append(column_1)
+            gname.append(column_2)
+            platform.append(column_3)
+            publisher.append(column_4)
+            developer.append(column_5)
+            critic_score.append(column_6)
+            sales_gl.append(column_7)
+            sales_na.append(column_8)
+            sales_eu.append(column_9)
+            sales_jp.append(column_10)
+            sales_ot.append(column_11)
+
+            rec_count += 1
+
+        except:
+            print('Got Exception')
+            continue
+
+# Formatting columns for the database
+columns = {'Rank': rank, 'Name': gname, 'Platform': platform, 'Publisher': publisher, 'Developer': developer, 'Critic_Score': critic_score,
+           'Global_Sales': sales_gl, 'NA_Sales': sales_na, 'EU_Sales': sales_eu, 'JP_Sales': sales_jp, 'Other_Sales': sales_ot}
+
+print (rec_count)
+
+df = pd.DataFrame(columns)
+
+# Converts all string "N/A" values in columns to proper NaN values
+df = df.replace("N/A", np.nan)
+
+# Converting score column and sales columns to numeric columns
+df[["Critic_Score", "Global_Sales", "NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales"]] = df[["Critic_Score", "Global_Sales", "NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales"]].apply(pd.to_numeric)
+print(df)
+df = df[['Rank', 'Name', 'Platform', 'Publisher','Developer', 'Critic_Score',
+         'Global_Sales', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales',
+         ]]
+df.index.name = None
+df.to_csv("vgsales2021.csv", sep=",", encoding='utf-8')
